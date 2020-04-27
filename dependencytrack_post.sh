48d68e4c44336f1120073d461b4e8a26d54477e7
abdelsfane <abdelsyfane@gmail.com>
2020-04-27 18:25:18 -0500

    init commit

---
+++ dependencytrack_post.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+cd ~/$PROJECT_NAME/${SPRING_APP}/build/libs/reports
+b64="$(cat bom.xml | base64)"
+
+curl -X "PUT" "http://18.222.135.38:8080/api/v1/bom" \
+     -H 'Content-Type: application/json' \
+     -H "X-API-Key: ${DT_TOKEN}" \
+     -d "{\"project\": \"${project_id}\",\"bom\": \"${b64}\"}"
+
